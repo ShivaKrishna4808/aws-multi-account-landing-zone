f64d3c17701366124748c2e96f904e68fbceae5a
ShivaKrishna4808 <shivakrish.tech68@gmail.com>
2026-09-23 12:23:11 -0700

    feat: add GitHub Actions apply role for account provisioning

--- modules/github-oidc/main.tf
+++ modules/github-oidc/main.tf
@@ -4,7 +4,14 @@ data "aws_iam_openid_connect_provider" "github" {
 
 locals {
   github_pr_subject = "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:pull_request"
+
+  github_main_subject = "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:ref:refs/heads/main"
 }
+
+# ============================================================
+# GitHub Actions PLAN role
+# Used only by Pull Requests
+# ============================================================
 
 data "aws_iam_policy_document" "github_actions_trust" {
   statement {
@@ -57,7 +64,6 @@ resource "aws_iam_role" "github_actions_plan" {
 }
 
 data "aws_iam_policy_document" "github_actions_permissions" {
-
   statement {
     sid    = "ReadTerraformStateBucket"
     effect = "Allow"
@@ -106,4 +112,123 @@ resource "aws_iam_role_policy" "github_actions_plan" {
   role = aws_iam_role.github_actions_plan.id
 
   policy = data.aws_iam_policy_document.github_actions_permissions.json
+}
+
+# ============================================================
+# GitHub Actions APPLY role
+# Used only from main branch
+# ============================================================
+
+data "aws_iam_policy_document" "github_actions_apply_trust" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type = "Federated"
+
+      identifiers = [
+        data.aws_iam_openid_connect_provider.github.arn
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+
+      values = [
+        "sts.amazonaws.com"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+
+      values = [
+        local.github_main_subject
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_apply" {
+  name = "LandingZoneGitHubActionsApplyRole"
+
+  assume_role_policy = data.aws_iam_policy_document.github_actions_apply_trust.json
+
+  max_session_duration = 3600
+
+  tags = {
+    Project   = "aws-multi-account-landing-zone"
+    ManagedBy = "Terraform"
+    Purpose   = "GitHubActionsAccountProvisioning"
+  }
+}
+
+data "aws_iam_policy_document" "github_actions_apply_permissions" {
+  statement {
+    sid    = "TerraformStateBucket"
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.terraform_state_bucket}"
+    ]
+  }
+
+  statement {
+    sid    = "TerraformStateObjects"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.terraform_state_bucket}/*"
+    ]
+  }
+
+  statement {
+    sid    = "AccountFactory"
+    effect = "Allow"
+
+    actions = [
+      "organizations:CreateAccount",
+      "organizations:DescribeAccount",
+      "organizations:DescribeCreateAccountStatus",
+      "organizations:DescribeOrganization",
+      "organizations:ListAccounts",
+      "organizations:ListAccountsForParent",
+      "organizations:ListChildren",
+      "organizations:ListOrganizationalUnitsForParent",
+      "organizations:ListParents",
+      "organizations:ListRoots",
+      "organizations:ListTagsForResource",
+      "organizations:MoveAccount",
+      "organizations:TagResource",
+      "organizations:UntagResource"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions_apply" {
+  name = "LandingZoneGitHubActionsApplyPolicy"
+  role = aws_iam_role.github_actions_apply.id
+
+  policy = data.aws_iam_policy_document.github_actions_apply_permissions.json
 }
